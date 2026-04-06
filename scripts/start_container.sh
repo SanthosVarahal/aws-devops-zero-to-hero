792716075cf5153e39328f27ac3dd923896d7714
SanthosVarahal <santoshvarahal@gmail.com>
2026-04-06 07:20:16 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull santhoshvarahal/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 santhoshvarahal/simple-python-flask-app 
+docker run -d -p 5000:5000 santhoshvarahal/simple-python-flask-app
